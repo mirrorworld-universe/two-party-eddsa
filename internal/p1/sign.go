6f50de680b763d95ace70df26f4826b5a2e7b6b3
Le Li <lile@rct.ai>
2022-10-09 10:00:14 +0800

    p1 sign done

--- internal/p1/sign.go
+++ internal/p1/sign.go
@@ -1,1 +1,101 @@
 package p1
+
+import (
+	"errors"
+	"main/internal/eddsa"
+	"math/big"
+)
+
+func Sign(serverKeypair *eddsa.Keypair, keyAgg *eddsa.KeyAgg) {
+	//clientKeypair, keyAgg := tempLoadKey()
+	println("serverKeypair=", serverKeypair.ToString(), " keyAgg=", keyAgg.ToString())
+
+	temp1 := [32]byte{
+		196, 206, 142, 211, 33, 209, 253, 148,
+		123, 34, 148, 86, 59, 179, 45, 242,
+		225, 141, 253, 97, 58, 156, 84, 142,
+		123, 77, 96, 1, 160, 16, 53, 164,
+	}
+	clientCommitment := new(big.Int).SetBytes(temp1[:])
+	temp2 := [32]byte{
+		44, 242, 77, 186, 95, 176, 163, 14,
+		38, 232, 59, 42, 197, 185, 226, 158,
+		27, 22, 30, 92, 31, 167, 66, 94,
+		115, 4, 51, 98, 147, 139, 152, 36,
+	}
+	msgHash := new(big.Int).SetBytes(temp2[:])
+	println("msgHash=", msgHash.String(), " clientCommitment=", clientCommitment.String())
+
+	eight := eddsa.ECSFromBigInt(new(big.Int).SetInt64(8))
+	eightInverse := eight.ModInvert()
+
+	temp3 := [32]byte{
+		55, 94, 156, 213, 171, 71, 43, 180,
+		25, 210, 117, 204, 69, 176, 139, 18,
+		223, 232, 74, 151, 45, 31, 77, 169,
+		236, 104, 205, 15, 156, 87, 239, 134,
+	}
+	clientPubkey := eddsa.ECPFromBytes(&temp3)
+	clientPubkey = clientPubkey.ECPMul(&eightInverse.Fe)
+	println("clientPubkey=", clientPubkey.ToString())
+
+	// round 1
+
+	serverEphemeralKey, serverSignFirstMsg, serverSignSecondMsg := eddsa.CreateEphemeralKeyAndCommit(serverKeypair, msgHash.Bytes())
+	println("serverEphemeralKey=", serverEphemeralKey.ToString(), ", serverSignFirstMsg=", serverSignFirstMsg.ToString()+", serverSignSecondMsg=", serverSignSecondMsg.ToString())
+
+	// now send clientSignFirstMsg, msgHash, client public key to p1, and receive serverFirstSignMsg
+
+	// round 2
+
+	clientSignSecondMsgRBytes := [32]byte{
+		157, 128, 104, 28, 9, 210, 124, 222,
+		144, 57, 146, 136, 200, 8, 228, 125,
+		119, 216, 89, 255, 247, 124, 203, 150,
+		67, 246, 66, 228, 108, 121, 46, 246,
+	}
+	clientSignSecondMsgR := eddsa.ECPFromBytes(&clientSignSecondMsgRBytes)
+	clientSignSecondMsgR = clientSignSecondMsgR.ECPMul(&eightInverse.Fe)
+
+	temp1 = [32]byte{
+		169, 43, 89, 150, 255, 113, 182, 143,
+		232, 177, 192, 27, 76, 61, 36, 72,
+		121, 68, 213, 61, 241, 206, 20, 165,
+		112, 33, 80, 6, 72, 206, 30, 83,
+	}
+	clientSignSecondMsgBF := new(big.Int).SetBytes(temp1[:])
+	println("round2, clientSignSecondMsgR=", clientSignSecondMsgR.ToString(), ", clientSignSecondMsgBF=", clientSignSecondMsgBF.String())
+
+	// check commiment
+	isCommMatch := eddsa.CheckCommitment(
+		clientSignSecondMsgR,
+		clientSignSecondMsgBF,
+		clientCommitment,
+	)
+
+	if !isCommMatch {
+		panic(errors.New("commitment not match"))
+	}
+
+	ri := []eddsa.Ed25519Point{
+		serverSignSecondMsg.R,
+		*clientSignSecondMsgR,
+	}
+	rTot := eddsa.SigGetRTot(ri)
+	println("rTot=", rTot.ToString())
+
+	msgHashBytes := msgHash.Bytes()
+	k := eddsa.SigK(rTot, &keyAgg.Apk, &msgHashBytes)
+	println("k=", k.ToString())
+
+	s1 := eddsa.PartialSign(
+		&serverEphemeralKey.SmallR,
+		serverKeypair,
+		&k,
+		&keyAgg.Hash,
+		rTot,
+	)
+	println("rTot=", rTot.ToString(), "k=", k.ToString(), "s1=", s1.ToString())
+
+	// send to client
+}
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"main/internal/eddsa"
 	"main/internal/p0"
+	"main/internal/p1"
+	"math/big"
 )
 
 func toLittleEdian(a []byte) (b []byte) {
@@ -20,14 +23,21 @@ func main() {
 	//publicKey, privateKey, _ := eddsa.GenerateKey(reader)
 	//fmt.Println(toLittleEdian(publicKey), privateKey)
 
-	//clientKeypair, keyAgg := p0.KeyGen()
-
-	println("\n\n************ SIGN now *************")
-	msg := "hello"
+	clientKeypair, keyAgg := p0.KeyGen()
+	println("clientKeypair=", clientKeypair)
+	//
+	//println("\n\n************ SIGN now *************")
+	//msg := "hello"
 	//p0.Sign(&msg, clientKeypair, keyAgg)
 
-	R := "1dd9ad91d660e104fc02043e7bbe0c303f1bfc1c012689ab8c2d38c4ae6be0e7"
-	s := "7bf0d2eb8027a65988c43a4c79e70f3ab67eadf1a8a852b5cf34ef1ace192407"
-	pubkey := "790c23f4a2f065fa4cebf77a005f75ad7a528c8de4ca64e4e5c681c17663514e"
-	p0.Verify(&msg, &R, &s, &pubkey)
+	//R := "1dd9ad91d660e104fc02043e7bbe0c303f1bfc1c012689ab8c2d38c4ae6be0e7"
+	//s := "7bf0d2eb8027a65988c43a4c79e70f3ab67eadf1a8a852b5cf34ef1ace192407"
+	//pubkey := "790c23f4a2f065fa4cebf77a005f75ad7a528c8de4ca64e4e5c681c17663514e"
+	//p0.Verify(&msg, &R, &s, &pubkey)
+	bn, _ := new(big.Int).SetString("2D282E87852DE00E981EFAAD08A9F435CF41CDFD7BD9EB3DBFBE08AE5537160", 16)
+	serverKeypair := eddsa.CreateKeyPairFromSeed(bn)
+	println("serverKeypair=", serverKeypair.ToString())
+
+	p1.Sign(serverKeypair, keyAgg)
+
 }
